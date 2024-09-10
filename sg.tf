bf60e095785494a4b427e3ecaf5b8415ebf80e43
takles22 <takles_2002@hotmail.com>
2024-09-10 22:22:07 +0300

    sg and add variable

--- sg.tf
+++ sg.tf
@@ -1,1 +1,24 @@
+resource "aws_security_group" "sg_HA" {
+  
+  name        = "sg-AH-3-tire"
+  vpc_id      = aws_vpc.vpc.id
+  dynamic "ingress" {
+    for_each = var.ingree_port
+    content {
+        from_port        = var.ingree_port[each.value]
+        to_port          = var.ingree_port[each.value]
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }   
+  }
 
+    dynamic "egress" {
+    for_each = var.egree_port
+    content {
+        from_port        = var.egree_port[each.value]
+        to_port          = var.egree_port[each.value]
+        protocol         = "-1"
+        cidr_blocks      = ["0.0.0.0/0"]
+    }   
+  }
+}
