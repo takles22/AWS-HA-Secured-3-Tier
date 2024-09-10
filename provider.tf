e7b47b3ed25a9009ed57f8ba8e1e9bbfe8ca3521
takles22 <takles_2002@hotmail.com>
2024-09-10 13:57:34 +0300

    first commit

--- provider.tf
+++ provider.tf
@@ -1,1 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.66.0"
+    }
+  }
+  backend "s3" {
+    bucket = "dolfined-team2024"
+    key = "HA-3tier/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+
+  profile = "terraform-dev"
+  region  = "us-east-1"
+
+}
+
+
 
