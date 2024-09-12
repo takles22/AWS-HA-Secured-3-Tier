184101281f11a15f63e5afa6c0769a22657bb38a
Mohamed Sayed <53582383+Mo-sayed0@users.noreply.github.com>
2024-09-12 13:40:51 +0300

    Update variable.tf by Mohamed

--- variable.tf
+++ variable.tf
@@ -8,13 +8,23 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 
-variable "availability_zone" {
-  default = {
-    "availability_zone_1a" = "us-east-1a"
-    "availability_zone_1b" = "us-east-1b"
-    "availability_zone_1c" = "us-east-1c"
-    "availability_zone_1d" = "us-east-1d"
-    "availability_zone_1e" = "us-east-1e"
-    "availability_zone_1f" = "us-east-1f"
-  }
+# Define the number of private subnets
+variable "private_subnet_count" {
+  description = "Number of private subnets"
+  type        = number
+  default     = 4
 }
+
+variable "public_subnet_count" {
+  description = "Number of Public subnets"
+  type        = number
+  default     = 2
+}
+
+# List of availability zones
+variable "availability_zones" {
+  description = "List of availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
